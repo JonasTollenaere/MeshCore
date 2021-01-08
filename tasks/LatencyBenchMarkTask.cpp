2a3488db63d31d7b1918f5c519f787337d546df4
Jonas Tollenaere <tollenaere.jonas@gmail.com>
2021-01-08 11:12:09 +0100

    Fixes in the cuda latency test

--- tasks/LatencyBenchMarkTask.cpp
+++ tasks/LatencyBenchMarkTask.cpp
@@ -5,17 +5,26 @@
 #include "LatencyBenchMarkTask.h"
 #include <cuda_runtime.h>
 
+// Larger data transfers, 128 kBytes
+//#define DSIZE 32 * 1024
+//#define LOOPS 100000
+
+// Small data transfers, 128 bytes
 #define DSIZE 32
 #define LOOPS 100000
 
 void LatencyBenchMarkTask::run() {
     {
-        auto startmicros = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-        int *d_data, *h_data;
+        int *d_data;
 
+        int *h_data;
         cudaMallocHost(&h_data, DSIZE*sizeof(int));
+//        int h_data[DSIZE];
+
+        cudaFree(nullptr);
         cudaMalloc(&d_data, DSIZE*sizeof(int));
 
+        auto startmicros = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
         for (int i=0; i < LOOPS; i++)
             cudaMemcpy(h_data, d_data, DSIZE*sizeof(int), cudaMemcpyDeviceToHost);
         auto stopmicros = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
