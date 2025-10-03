a313816628704b83f3778882ab2ed3fa31ecd2f6
Jonas Tollenaere <jonas.tollenaere@kuleuven.be>
2025-10-03 11:56:57 +0200

    Potential fix for Qt plugin deployment when developing

---
+++ demos/dummy.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Jonas on 27/08/2025.
+//
+
+#include <QApplication>
+
+int main(int argc, char *argv[]){
+
+    QApplication app(argc, argv);
+
+    int returnCode = QApplication::exec();
+    return returnCode;
+}
