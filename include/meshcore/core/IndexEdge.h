243ffeb5967afa7e87bc7edc5214e04b98fabda7
Jonas Tollenaere <jonas.tollenaere@kuleuven.be>
2025-11-05 17:07:43 +0100

    Rework IndexEdge for simplified hashing

--- include/meshcore/core/IndexEdge.h
+++ include/meshcore/core/IndexEdge.h
@@ -5,8 +5,50 @@
 #ifndef MESHCORE_INDEXEDGE_H
 #define MESHCORE_INDEXEDGE_H
 
+#include <algorithm>
+
+/**
+ * @brief Represents an (undirected) edge as a pair of vertex indices.
+ *
+ * IndexEdge stores the two vertex indices in canonical (non-decreasing)
+ * order: vertexIndex0 <= vertexIndex1. The constructor normalizes the
+ * provided indices so that the same undirected edge (a,b) and (b,a)
+ * produce identical IndexEdge objects.
+ *
+ * Invariants:
+ * - vertexIndex0 and vertexIndex1 are immutable (const) after construction.
+ * - vertexIndex0 <= vertexIndex1 (canonical ordering).
+ *
+ * Rationale / usage notes:
+ * - Because indices are canonicalized on construction, equality is a
+ *   simple member-wise comparison (see operator==).
+ * - To use IndexEdge as a key in unordered containers you may either
+ *   provide a `std::hash<IndexEdge>` specialization or pass a custom
+ *   hasher. Since the indices are canonical, a straightforward hash is to
+ *   pack the two indices (e.g. ((uint64_t)vertexIndex0 << 32) | vertexIndex1)
+ *   and hash that value.
+ *
+ * Example:
+ *   IndexEdge e(5, 2); // stored as (2,5)
+ */
 struct IndexEdge{
     const size_t vertexIndex0;
     const size_t vertexIndex1;
+
+    IndexEdge(const size_t vertexIndex0, const size_t vertexIndex1):
+    vertexIndex0(std::min(vertexIndex0, vertexIndex1)),
+    vertexIndex1(std::max(vertexIndex0, vertexIndex1))
+    {}
 };
+
+// Inline equality and inequality operators. IndexEdge stores indices in a canonical
+// (min, max) order, so equality is a simple member-wise comparison.
+constexpr bool operator==(const IndexEdge &a, const IndexEdge &b) noexcept {
+    return a.vertexIndex0 == b.vertexIndex0 && a.vertexIndex1 == b.vertexIndex1;
+}
+
+constexpr bool operator!=(const IndexEdge &a, const IndexEdge &b) noexcept {
+    return !(a == b);
+}
+
 #endif //MESHCORE_INDEXEDGE_H
--- include/meshcore/utility/hash.h
+++ include/meshcore/utility/hash.h
@@ -9,11 +9,12 @@
 #include "meshcore/core/Sphere.h"
 #include "meshcore/core/Plane.h"
 #include "meshcore/core/Ray.h"
+#include "meshcore/core/IndexEdge.h"
 
 #if GLM_HAS_CXX11_STL
     #include <glm/gtx/hash.hpp>
     template<> struct std::hash<VertexTriangle> {
-        size_t operator()(const VertexTriangle &vertexTriangle) const {
+        size_t operator()(const VertexTriangle &vertexTriangle) const noexcept {
             size_t result = 0;
             auto hash = std::hash<Vertex>();
             glm::detail::hash_combine(result, hash(vertexTriangle.vertices[0]));
@@ -24,7 +25,7 @@
     };
 
     template<> struct std::hash<AABB> {
-        size_t operator()(const AABB &aabb) const {
+        size_t operator()(const AABB &aabb) const noexcept {
             size_t result = 0;
             auto hash = std::hash<Vertex>();
             glm::detail::hash_combine(result, hash(aabb.getMinimum()));
@@ -34,7 +35,7 @@
     };
 
     template<> struct std::hash<Sphere> {
-        size_t operator()(const Sphere &sphere) const {
+        size_t operator()(const Sphere &sphere) const noexcept {
             size_t result = 0;
             auto vertexHash = std::hash<Vertex>();
             auto floatHash = std::hash<float>();
@@ -45,7 +46,7 @@
     };
 
     template<> struct std::hash<Plane> {
-        size_t operator()(const Plane &plane) const {
+        size_t operator()(const Plane &plane) const noexcept {
             size_t result = 0;
             auto vectorHash = std::hash<glm::vec3>();
             auto floatHash = std::hash<float>();
@@ -56,7 +57,7 @@
     };
 
     template<> struct std::hash<Ray> {
-        size_t operator()(const Ray &ray) const {
+        size_t operator()(const Ray &ray) const noexcept {
             size_t result = 0;
             auto vertexHash = std::hash<Vertex>();
             auto vectorHash = std::hash<glm::vec3>();
@@ -65,18 +66,31 @@
             return result;
         }
     };
+
+    template<> struct std::hash<IndexEdge>
+    {
+        size_t operator()(const IndexEdge &indexEdge) const noexcept
+        {
+            size_t result = 0;
+            auto intHash = std::hash<size_t>();
+            glm::detail::hash_combine(result, intHash(indexEdge.vertexIndex0));
+            glm::detail::hash_combine(result, intHash(indexEdge.vertexIndex1));
+            return result;
+        }
+    };
+
 #else
     #include <functional>
 
 
     template<> struct std::hash<Vertex> {
-        size_t operator()(const Vertex &vertex) const {
+        size_t operator()(const Vertex &vertex) const noexcept {
             return std::hash<float>()(vertex.x +  vertex.y + vertex.z);
         }
     };
 
     template<> struct std::hash<VertexTriangle> {
-        size_t operator()(const VertexTriangle &vertexTriangle) const {
+        size_t operator()(const VertexTriangle &vertexTriangle) const noexcept {
             return std::hash<float>()(vertexTriangle.vertices[0].x +  vertexTriangle.vertices[0].y + vertexTriangle.vertices[0].z +
                                       vertexTriangle.vertices[1].x +  vertexTriangle.vertices[1].y + vertexTriangle.vertices[1].z +
                                       vertexTriangle.vertices[2].x +  vertexTriangle.vertices[2].y + vertexTriangle.vertices[2].z);
@@ -84,7 +98,7 @@
     };
 
     template<> struct std::hash<AABB> {
-        size_t operator()(const AABB &aabb) const {
+        size_t operator()(const AABB &aabb) const noexcept {
             const auto& min = aabb.getMinimum();
             const auto& max = aabb.getMaximum();
             return std::hash<float>()(min.x + min.y + min.z + max.x + max.y + max.z);
@@ -92,20 +106,32 @@
     };
 
     template<> struct std::hash<Sphere> {
-        size_t operator()(const Sphere &sphere) const {
+        size_t operator()(const Sphere &sphere) const noexcept {
             return std::hash<float>()(sphere.getCenter().x + sphere.getCenter().y + sphere.getCenter().z + sphere.getRadius());
         }
     };
 
     template<> struct std::hash<Plane> {
-        size_t operator()(const Plane &plane) const {
+        size_t operator()(const Plane &plane) const noexcept {
             return std::hash<float>()(plane.getNormal().x + plane.getNormal().y + plane.getNormal().z + plane.getD());
         }
     };
 
     template<> struct std::hash<Ray> {
-        size_t operator()(const Ray &ray) const {
+        size_t operator()(const Ray &ray) const noexcept {
             return std::hash<float>()(ray.getOrigin().x + ray.getOrigin().y + ray.getOrigin().z + ray.getDirection().x + ray.getDirection().y + ray.getDirection().z);
+        }
+    };
+
+    // Fallback specialization for IndexEdge so static analyzers (clangd) see it
+    // even when GLM_HAS_CXX11_STL isn't defined for the analyzer's configuration.
+    template<> struct std::hash<IndexEdge> {
+        size_t operator()(const IndexEdge &indexEdge) const noexcept {
+            auto h = std::hash<size_t>();
+            // simple but effective combine
+            size_t a = h(indexEdge.vertexIndex0);
+            size_t b = h(indexEdge.vertexIndex1);
+            return a ^ (b + 0x9e3779b97f4a7c15ULL + (a<<6) + (a>>2));
         }
     };
 #endif
